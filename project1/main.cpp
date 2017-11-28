b5c717ac2ce7bc6d2d970e8207677fb733a72878
Arnarish <34062969+Arnarish@users.noreply.github.com>
2017-11-28 13:49:09 +0000

    Add files via upload
    Verkefni 1A

--- project1/main.cpp
+++ project1/main.cpp
@@ -1,8 +1,46 @@
 #include <iostream>
+#include <fstream>
+
 using namespace std;
 
-int main(void)
+int main()
 {
+    char cont;
+    bool quit = false;
+    string str;
+    ifstream fin;
+    fin.open("Loren ipsum.txt");
+    if(fin.is_open())
+    {
+            while(!fin.eof())
+            {
+                cont = 'a';
+                for(int i=0; i < 10; i++)
+                {
+                    getline(fin, str);
+                    cout << str << endl;
+                }
+                cout << "Do you wish to continue? (Y/N)" << endl;
+                cin >> cont;
+                while(cont != 'y' && cont != 'Y')
+                {
+                    if(cont == 'n' || cont == 'N')
+                    {
+                        quit = true;
+                        break;
+                    }
+                    cout << "Incorrect input. Do you wish to continue? (Y/N)" << endl;
+                    cin >> cont;
+                }
+                if (quit) break;
+            }
+            fin.close();
+    }
+    else
+    {
+        cout << "Unable to read from file!" << endl;
+    }
+
+
     return 0;
 }
-
