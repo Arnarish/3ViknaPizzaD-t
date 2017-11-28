e1f0196a7b73c1b62acc78c23842c871a086d633
magician <odinn08@gmail.com>
2017-11-28 14:19:16 +0000

    Slight changes to project1

--- project1/main.cpp
+++ project1/main.cpp
@@ -9,32 +9,32 @@ int main()
     bool quit = false;
     string str;
     ifstream fin;
-    fin.open("Loren ipsum.txt");
+    fin.open("test_file.txt");
     if(fin.is_open())
     {
-            while(!fin.eof())
+        while(!fin.eof())
+        {
+            for(int i = 0; i < 10; i++)
             {
-                cont = 'a';
-                for(int i=0; i < 10; i++)
-                {
-                    getline(fin, str);
-                    cout << str << endl;
-                }
-                cout << "Do you wish to continue? (Y/N)" << endl;
-                cin >> cont;
-                while(cont != 'y' && cont != 'Y')
-                {
-                    if(cont == 'n' || cont == 'N')
-                    {
-                        quit = true;
-                        break;
-                    }
-                    cout << "Incorrect input. Do you wish to continue? (Y/N)" << endl;
-                    cin >> cont;
-                }
-                if (quit) break;
+                getline(fin, str);
+                cout << str << endl;
             }
-            fin.close();
+            cout << "Do you wish to continue? (Y/N)" << endl;
+            cin >> cont;
+            while(cont != 'y' && cont != 'Y')
+            {
+                if (cont == 'n' || cont == 'N')
+                {
+                    quit = true;
+                    break;
+                }
+                cout << "Incorrect input. Do you wish to continue? (Y/N)" << endl;
+                cin >> cont;
+            }
+            if (quit) 
+                break;
+        }
+        fin.close();
     }
     else
     {
