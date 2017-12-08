3ac7f0e2a5879fd4ce92e3006e87984dd8633b7d
Arnarish <34062969+Arnarish@users.noreply.github.com>
2017-12-08 18:07:44 +0000

    added incomplete pizza model

---
+++ main/models/pizza.h
@@ -0,0 +1,19 @@
+#ifndef PIZZA_H
+#define PIZZA_H
+
+
+class pizza
+{
+    public:
+        pizza();
+        ~pizza();
+
+    private:
+        enum base;
+        //vector or array of toppings?
+        //split them by meat/veggie?
+        //option to split pizzas? needs to be implemented higher up
+
+};
+
+#endif // PIZZA_H
