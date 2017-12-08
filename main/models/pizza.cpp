c36a59bc19461d6907dd2ef77e01f26a874fea65
Arnarish <34062969+Arnarish@users.noreply.github.com>
2017-12-08 16:57:36 +0000

    Started on model classes

---
+++ main/models/pizza.cpp
@@ -0,0 +1,14 @@
+#include "pizza.h"
+enum base {
+    normal = 1, thin, spelt; // use enum for limited toppings/options? e.g. cheese no cheese, sauce, no sauce, e.t.c
+}
+
+pizza::pizza()
+{
+    //ctor
+}
+
+pizza::~pizza()
+{
+    //dtor
+}
---
+++ main/models/pizza.h
@@ -0,0 +1,15 @@
+#ifndef PIZZA_H
+#define PIZZA_H
+
+
+class pizza
+{
+    public:
+        pizza();
+        ~pizza();
+
+    private:
+        enum base;
+};
+
+#endif // PIZZA_H
